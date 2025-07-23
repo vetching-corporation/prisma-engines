7c75b1c79990b6949254b87f4e259242cf5c1ed2
Seongsu <nfl1ryxditimo12@gmail.com>
2025-07-23 18:06:26 +0900

    feat(query-compiler-wasm): add dynamic schema support (#5)
    * chore: 기존 워크플로우 제거
    
    * feat: 새로운 배포 워크플로우 추가
    
    * feat: Prisma 6.12.0 Custom Engine

--- query-compiler/query-compiler/src/lib.rs
+++ query-compiler/query-compiler/src/lib.rs
@@ -11,7 +11,7 @@ use quaint::{
     visitor,
 };
 use query_core::{Operation, QueryGraphBuilderError, schema::QuerySchema};
-use sql_query_builder::{Context, SqlQueryBuilder};
+use sql_query_builder::{Context, DynamicSchema, SqlQueryBuilder};
 use thiserror::Error;
 pub use translate::{TranslateError, translate};
 
@@ -34,7 +34,16 @@ pub fn compile(
     query: Operation,
     connection_info: &ConnectionInfo,
 ) -> Result<Expression, CompileError> {
-    let ctx = Context::new(connection_info, None);
+    compile_with_dynamic_schema(query_schema, query, connection_info, DynamicSchema::default())
+}
+
+pub fn compile_with_dynamic_schema(
+    query_schema: &QuerySchema,
+    query: Operation,
+    connection_info: &ConnectionInfo,
+    dynamic_schema: DynamicSchema,
+) -> Result<Expression, CompileError> {
+    let ctx = Context::new_with_dynamic_schema(connection_info, dynamic_schema, None);
     let graph = QueryGraphBuilder::new(query_schema).build(query)?;
 
     let res: Result<Expression, TranslateError> = match connection_info.sql_family() {
